33216de03d5a5926160b27865da982e4611eee84
jerr-gith <jerry.onlinecommunities@gmail.com>
2024-07-12 16:02:10 +0100

    Add MarkDown Editor

--- app/bugs/new/page.tsx
+++ app/bugs/new/page.tsx
@@ -1,11 +1,15 @@
+"use client"
+
 import { Button, TextArea, TextField } from "@radix-ui/themes";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 const NewBugPage = () => {
   return (
     <div className="space-y-4">
       <h2 className="">New Bug Page</h2>
       <TextField.Root variant="surface" placeholder="What's the bug…" />
-      <TextArea placeholder="Describe the bug…" />
+      <SimpleMDE placeholder="Describe the bug…" />
       <Button>Submit New Bug</Button>
     </div>
   );
